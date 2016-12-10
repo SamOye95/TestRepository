4e4ab5279c81782232b9972095d2c67f3da5a255
Samuel Oyediran <a.s.m.oyediran@student.utwente.nl>
2016-12-10 13:30:41 +0100

    Initial Commit

---
+++ TestlGit/src/Test/git/Hello.java
@@ -0,0 +1,10 @@
+package Test.git;
+
+public class Hello {
+	 public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+
+}
